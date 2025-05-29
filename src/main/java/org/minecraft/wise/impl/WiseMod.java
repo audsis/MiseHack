94e70f534541eec800b884e9a6ef18b17d0e44ad
audsis <101012571+audsis@users.noreply.github.com>
2025-05-29 18:20:47 +0300

    Update WiseMod.java

--- src/main/java/org/minecraft/wise/impl/WiseMod.java
+++ src/main/java/org/minecraft/wise/impl/WiseMod.java
@@ -12,9 +12,9 @@ import net.minecraft.util.Identifier;
 public class WiseMod {
 
     public static final String MOD_ID = "minecraft";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "2.0.0";
     public static final String NAME_UNICODE = "☄";
-    public static String NAME = "Wisehack";
+    public static String NAME = "Wisehack.net by James";
     public static final String NAME_VERSION = NAME + " " + GitInfo.VERSION + "+" + GitInfo.GIT_REVISION + "." + GitInfo.GIT_SHA.substring(0, 10);
     public static NotificationProcessor notificationProcessor;
 
